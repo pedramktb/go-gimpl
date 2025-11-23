5f7399144dc86d6333a2b152954f7eb86ecd1894
pedramktb <79080845+pedramktb@users.noreply.github.com>
2025-11-23 14:27:36 +0100

    refactor: Simplify mock implementations by separating generic mock types for better clarity

--- mock.go
+++ mock.go
@@ -6,51 +6,47 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
-// A mock that mocks all generic Mock interfaces for Entity E, CreateEntity C, UpdateEntity U, and ReplaceEntity R
-// To be used in tests
-type Mock[E Entity, C CreateEntity, U UpdateEntity, CU CreateOrUpdateEntity] struct{ mock.Mock }
+var _ Get[Entity] = (&MockGetter[Entity]{}).Get
 
-func (m *Mock[E, C, U, CU]) Get(ctx context.Context, locateOpts []LocateOpt, paginateOpts []PaginateOpt, txOpts ...TxOpt) (Paginated[E], error) {
+type MockGetter[E Entity] struct{ mock.Mock }
+
+func (m *MockGetter[E]) Get(ctx context.Context, locateOpts []LocateOpt, paginateOpts []PaginateOpt, txOpts ...TxOpt) (Paginated[E], error) {
 	args := m.Called(ctx, locateOpts, paginateOpts)
 	return args.Get(0).(Paginated[E]), args.Error(1)
 }
-func (m *Mock[E, C, U, CU]) Create(ctx context.Context, items []C, txOpts ...TxOpt) error {
+
+var _ Create[CreateEntity] = (&MockCreator[CreateEntity]{}).Create
+
+type MockCreator[C CreateEntity] struct{ mock.Mock }
+
+func (m *MockCreator[C]) Create(ctx context.Context, items []C, txOpts ...TxOpt) error {
 	args := m.Called(ctx, items, txOpts)
 	return args.Error(0)
 }
-func (m *Mock[E, C, U, CU]) Update(ctx context.Context, items []U, txOpts ...TxOpt) error {
+
+var _ Update[UpdateEntity] = (&MockUpdater[UpdateEntity]{}).Update
+
+type MockUpdater[U UpdateEntity] struct{ mock.Mock }
+
+func (m *MockUpdater[U]) Update(ctx context.Context, items []U, txOpts ...TxOpt) error {
 	args := m.Called(ctx, items, txOpts)
 	return args.Error(0)
 }
-func (m *Mock[E, C, U, CU]) CreateOrUpdate(ctx context.Context, items []CU, txOpts ...TxOpt) error {
+
+var _ CreateOrUpdate[CreateOrUpdateEntity] = (&MockCreateOrUpdater[CreateOrUpdateEntity]{}).CreateOrUpdate
+
+type MockCreateOrUpdater[CU CreateOrUpdateEntity] struct{ mock.Mock }
+
+func (m *MockCreateOrUpdater[CU]) CreateOrUpdate(ctx context.Context, items []CU, txOpts ...TxOpt) error {
 	args := m.Called(ctx, items, txOpts)
 	return args.Error(0)
 }
-func (m *Mock[E, C, U, CU]) Delete(ctx context.Context, locateOpts []LocateOpt, txOpts ...TxOpt) error {
+
+var _ Delete[Entity] = (&MockDeleter[Entity]{}).Delete
+
+type MockDeleter[E Entity] struct{ mock.Mock }
+
+func (m *MockDeleter[E]) Delete(ctx context.Context, locateOpts []LocateOpt, txOpts ...TxOpt) error {
 	args := m.Called(ctx, locateOpts, txOpts)
 	return args.Error(0)
 }
-
-type e struct{}
-
-func (e) SortPtr(string) any   { return nil }
-func (e) FilterPtr(string) any { return nil }
-
-type c struct{}
-
-func (c) CreatePtrs() []any { return nil }
-
-type r struct{}
-
-func (r) ReplacePtrs() []any { return nil }
-
-type u struct{}
-
-func (u) UpdatePtrs() []any { return nil }
-
-// Ensure Mock implements all required interfaces
-var _ Get[e] = (&Mock[e, c, u, r]{}).Get
-var _ Create[c] = (&Mock[e, c, u, r]{}).Create
-var _ Update[u] = (&Mock[e, c, u, r]{}).Update
-var _ CreateOrUpdate[r] = (&Mock[e, c, u, r]{}).CreateOrUpdate
-var _ Delete[e] = (&Mock[e, c, u, r]{}).Delete
